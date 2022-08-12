a38286a657df4332c6b07bc0a6967fcdf3daee5c
Subanista Thaneswaran <88665143+Subanista@users.noreply.github.com>
2022-08-12 18:17:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('i am started to learn python.....');
+# no need to put the semicolon 
+print("hi, i am subanista ") #this also possible to write a comment 
+#comment will start with  "#"
+#indentation 
+if 3>2:
+ print("three is greater than two")
+ 
+print('suba')
