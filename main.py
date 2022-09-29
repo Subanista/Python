6017b92970030da5d8f6780268ffc254b225e098
Subanista Thaneswaran <88665143+Subanista@users.noreply.github.com>
2022-09-29 06:37:46 +0000

    starting to code in python

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 print('i am started to learn python.....');
 # no need to put the semicolon 
-print("hi, i am subanista ") #this also possible to write a comment 
+print("hi, i am subanista ") #this also possible to write a statement
 #comment will start with  "#"
 #indentation 
 if 3>2:
  print("three is greater than two")
- 
-print('suba')
+ print("hello world")
+years = 1
+years = 2
+print(years)
+price = 345.50
+print(price)
+first_name = "Tamil "
+print(first_name)
+is_online = True
+print(is_online)
+name = input("enter your name ")
+print("hi " + name)
+birth_year = input("enter your birth year ")
+age = 2022 - int(birth_year)
+print("Hi " + name + ", your age is " + str(age))
+first = input("First number ")
+second = input("second number ")
+summation = float(first) + float(second)
+print("Sum is " + str(summation))
+pink = float(input("enter a value : "))
+green = float(input("enter a value : "))
+addition = pink + green
+print(addition)
+subject = "Information Technology"
+print(subject.upper())
+print(subject)
+print(subject.find("m"))
+print(subject.find("M"))
+print(subject.find("Technology"))
+print(subject.replace("Technology", "tech"))
+code = "coding is better hobby what i have"
+print(code)
